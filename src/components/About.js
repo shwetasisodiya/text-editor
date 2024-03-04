b8ef50a53c963a364ed906dd71d77ed36f5047f8
shwetasisodiya <sisodiyashweta5@gmail.com>
2024-03-04 10:54:24 +0530

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ export default function About(props) {
   <div className="card-body">
     <h5 className="card-title">About Us</h5>
     <p className="card-text">I'm Working individual who is creating website to get experiance in my work and get aknowledge by others. At TextEditor, I believe this website can help people in need.</p>
-    <a href="https://linkedin.com/in/dhirendra-sisodiya-a3aa00226" className="">
+    <a href="https://www.linkedin.com/in/shweta-sisodiya/" className="">
         <img src={`${Linkdin}`} alt="" />
         
     </a>
